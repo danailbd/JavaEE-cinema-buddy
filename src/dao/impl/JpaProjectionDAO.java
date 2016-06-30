771998d7c0fa3d4cfd9def21d82a2312debc943d
Vera Boycheva <vboi4eva@gmail.com>
2016-06-30 03:17:12 +0300

    deleted

---
+++ src/dao/impl/JpaProjectionDAO.java
@@ -0,0 +1,65 @@
+package dao.impl;
+
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+
+import dao.ProjectionDAO;
+import dao.models.Movie;
+import dao.models.Projection;
+
+public class JpaProjectionDAO extends JpaDAO implements ProjectionDAO {
+
+	@Override
+	public void addProjection(Projection projection) {
+		//checkTime(projection.getStartTime(), projection.getEndTime());
+		persist(projection);
+
+	}
+/*
+	private void checkTime(Date d, Date es) {
+		//List<Projection> projs = getAllProjections()
+		
+	}*/
+
+	@Override
+	public void removeProjection(int id) {
+		Projection projection = findById(id);
+		this.remove(projection);
+
+	}
+
+	@Override
+	public List<Projection> getAllProjections() {
+		return findAll();
+	}
+
+	@Override
+	public Projection getProjectionById(int id) {
+		return findById(id);
+	}
+
+	/*
+	 * @Override public int getSeatsByProjection(int id) { Projection projection
+	 * = getProjectionById(id); return projection.getBooking().getSeats(); }
+	 */
+	@Override
+	public List<Projection> getProjectionsByDate(Date date) {
+		TypedQuery<Projection> query = entityManager.createNamedQuery("getProjectionByDate", Projection.class)
+				.setParameter("DATE", date);
+		try {
+			return query.getResultList();
+		} catch (NoResultException e) {
+			return null;
+		}
+	};
+
+	@Override
+	public void updateProjection(Projection projection) {
+
+	}
+}
---
+++ src/rest/ProjectionResource.java
@@ -0,0 +1,114 @@
+package rest;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import dao.JpaBookingDAOFactory;
+import dao.impl.JpaProjectionDAO;
+import dao.models.Projection;
+
+@Path("/projection")
+public class ProjectionResource {
+	private static final Response RESPONSE_OK = Response.ok().build();
+	private static final Response RESPONSE_FAIL = Response.status(400).build();
+	private static final JpaBookingDAOFactory jpaProjectionDAOFactory = new JpaBookingDAOFactory();
+	private static JpaProjectionDAO dao = jpaProjectionDAOFactory.getJpaProjectionDAO();
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Projection> getAllProjections() {
+		try {
+			List<Projection> allProjections = dao.getAllProjections();
+			return allProjections;
+		} catch (Exception e) {
+			return null;
+		}
+	}
+	
+	@GET
+	@Path("date/{date}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Projection> getProjectionsByDate(@PathParam("date") Date date) {
+		try {
+			List<Projection> allProjections = dao.getProjectionsByDate(date);
+			return allProjections;
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	@GET
+	@Path("/id/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Projection getById(@PathParam("projectionId") Integer projectionId) {
+		try {
+			Projection projection = dao.getProjectionById(projectionId);
+			return projection;
+		} catch (Exception e) {
+			return null;
+		}
+	}
+	//TODO: 
+	@GET
+	@Path("/freeSeats/{projectionId}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public List<Integer> getSeatsById(@PathParam("projectionId") Integer projectionId) {
+		try {
+<<<<<<< HEAD
+			//String seats= dao.getProjectionSeats(projectionId);
+			return null;//seats;
+=======
+			List<Integer> seats= dao.getProjectionSeatsById(projectionId);
+			return seats;
+>>>>>>> refs/remotes/origin/master
+		} catch (Exception e) {
+			return null;
+		}
+	}
+	
+	@PUT
+	@Path("/add")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response addNewProjection(Projection projection) {
+		try {
+			dao.addProjection(projection);
+			return RESPONSE_OK;
+		}catch(Exception e){
+			return RESPONSE_FAIL;
+		}
+
+	};
+
+	@POST
+	@Path("/update")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response updateProjection(Projection projection) {
+		try {
+			dao.updateProjection(projection);
+			return RESPONSE_OK;
+		}catch(Exception e){
+			return RESPONSE_FAIL;
+		}
+	}
+
+	@POST
+	@Path("/delete/{projectionId}")
+	public Response deleteProjection(@PathParam("projectionId") Integer projectionId) {
+		try {
+			dao.removeProjection(projectionId);
+			return RESPONSE_OK;
+		} catch (Exception e) {
+			return RESPONSE_FAIL;
+		}
+	}
+}
